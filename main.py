ad39c4e9dc4341156387dc801f3078298c0f7066
Hadeed Zubair <60264256+HADEED-143@users.noreply.github.com>
2022-10-27 02:41:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 import pandas as pd
 import csv
-df = pd.read_csv('Hadeed-Task-Data (1).csv')
 
-agecsv18 = df[(df['age'] == 18)]
-agecsv19 = df[(df['age'] == 19)]
-agecsv20 = df[(df['age'] == 20)]
+df = pd.read_csv('Hadeed-Task-Data (1).csv', usecols=['age'])
+df.head()
+df.values.tolist()
+print(df)
+for row in df:
+    age_15 = df[(df['age'] == 15)]
+    print(age_15.to_csv('age_15.csv'))
 
-agecsv18.isnull()
-agecsv19.isnull()
-agecsv20.isnull()
+    age_16 = df[(df['age'] == 16)]
+    print(age_16.to_csv('age_16.csv'))
 
-agecsv18 = df.fillna(value=0)
-agecsv19 = df.fillna(value=0)
-agecsv20 = df.fillna(value=0)
+    age_17 = df[(df['age'] == 17)]
+    print(age_17.to_csv('age_17.csv'))
 
-print(agecsv18.head().to_csv('age_18.csv'))
-print(agecsv19.head().to_csv('age_19.csv'))
-print(agecsv20.head().to_csv('age_20.csv'))
+    age_18 = df[(df['age'] == 18)]
+    print(age_18.to_csv('age_18.csv'))
 
+    age_19 = df[(df['age'] == 19)]
+    print(age_19.to_csv('age_19.csv'))
 
+    age_20 = df[(df['age'] == 20)]
+    print(age_20.to_csv('age_20.csv'))
 
+    age_21 = df[(df['age'] == 21)]
+    print(age_21.to_csv('age_21.csv'))
 
+    age_22 = df[(df['age'] == 22)]
+    print(age_22.to_csv('age_22.csv'))
 
+    age_23 = df[(df['age'] == 23)]
+    print(age_23.to_csv('age_23.csv'))
+
+    age_24 = df[(df['age'] == 24)]
+    print(age_24.to_csv('age_24.csv'))
+
+else:
+    print("not in range data will not save in file")
 """
 age_18 = agecsv18.to_csv('age_18.csv')
 age_19 = agecsv19.to_csv('age_19.csv')
